ab04ff42790f69550dcc106100dfd6efe75f687c
rwilkett <Ray.Wilkett@RWILKETT.body.local>
2015-08-07 14:36:53 -0600

    Fix adb path to match sauce.

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-adb -s remount
-adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/blob/master/hosts /etc/hosts
+/home/chef/android-sdk-linux/platform-tools/adb -s remount
+/home/chef/android-sdk-linux/platform-tools/adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/blob/master/hosts /etc/hosts
