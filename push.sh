19b3978a2f072ab5d3482bc3dda753482646e205
rwilkett <Ray.Wilkett@RWILKETT.body.local>
2015-08-07 14:55:53 -0600

    Get rid of bin/sh

--- push.sh
+++ push.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 /home/chef/android-sdk-linux/platform-tools/adb -s remount
 /home/chef/android-sdk-linux/platform-tools/adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/blob/master/hosts /etc/hosts
