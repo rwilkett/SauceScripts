e07d398d19abf0ff64c17834deced9a16e05ef53
rwilkett <Ray.Wilkett@RWILKETT.body.local>
2015-08-07 14:26:16 -0600

    Fix path to hosts file.

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 adb -s remount
-adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/hosts /etc/hosts
+adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/blob/master/hosts /etc/hosts
