105fad45e7057c1d60b944b8267c557034f84299
rwilkett <Ray.Wilkett@RWILKETT.body.local>
2015-08-10 11:18:57 -0600

    Add shebang

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 /home/chef/android-sdk-linux/platform-tools/adb -s remount
 /home/chef/android-sdk-linux/platform-tools/adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/blob/master/hosts /etc/hosts
