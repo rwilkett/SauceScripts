8e44915d035483106f81053fc2571532fb868e61
rwilkett <Ray.Wilkett@RWILKETT.body.local>
2015-08-10 13:26:10 -0600

    Change URL of host file to raw

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /home/chef/android-sdk-linux/platform-tools/adb -s remount
-/home/chef/android-sdk-linux/platform-tools/adb -s emulator-5554 push https://github.com/rwilkett/SauceScripts/blob/master/hosts /etc/hosts
+/home/chef/android-sdk-linux/platform-tools/adb -s emulator-5554 push https://raw.githubusercontent.com/rwilkett/SauceScripts/master/hosts
