cbe32accdeb5f8462f2dd2cea4c1763371c8f92d
rwilkett <ray.wilkett@RWILKETT.body.local>
2015-08-19 15:45:44 -0600

    Fix

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$ANDROID_HOME/platform-tools/adb -s remount
+$ANDROID_HOME/platform-tools/adb -s emulator-5554 remount
 $ANDROID_HOME/platform-tools/adb -s emulator-5554 push "https://raw.githubusercontent.com/rwilkett/SauceScripts/master/hosts" /etc/hosts
